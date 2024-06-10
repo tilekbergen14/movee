0c67847e3b9afcb7dd944ada6e587178c06238dc
tq <tilekbergen.third@gmail.com>
2024-06-10 16:06:21 +0500

    editied some things

--- api/server.go
+++ api/server.go
@@ -28,11 +28,10 @@ func NewServer(Addr string, storage storage.Storage) *Server {
 func (s *Server) Run() error {
 
 	router := mux.NewRouter()
+	router.Use(enableCORS)
 
 	fs := http.FileServer(http.Dir("./assets/"))
 	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fs))
-	router.Use(enableCORS)
-	router.HandleFunc("/handlefile", makeHttpHandleFunc(s.handleFile))
 
 	router.HandleFunc("/", makeHttpHandleFunc(s.handleHome))
 	router.HandleFunc("/movies", makeHttpHandleFunc(s.handleMovies))
@@ -43,7 +42,7 @@ func (s *Server) Run() error {
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
 		next.ServeHTTP(w, r)
 	})
@@ -63,10 +62,15 @@ func (s *Server) handleMovies(w http.ResponseWriter, r *http.Request) error {
 	}
 	if r.Method == "POST" {
 		movie := &types.Movie{}
-
-		if err := json.NewDecoder(r.Body).Decode(movie); err != nil {
+		filepath, err := s.handleFile(r)
+		if err != nil {
 			return err
 		}
+
+		movie.Description = r.FormValue("description")
+		movie.Name = r.FormValue("name")
+		movie.Image = filepath
+
 		if err := s.Storage.CreateMovie(movie); err != nil {
 			return err
 		}
@@ -75,25 +79,22 @@ func (s *Server) handleMovies(w http.ResponseWriter, r *http.Request) error {
 	return writeJSON(w, http.StatusMethodNotAllowed, "Method not allowed!")
 }
 
-func (s *Server) handleFile(w http.ResponseWriter, r *http.Request) error {
-	if r.Method == "POST" {
-		r.ParseMultipartForm(32 << 20)
-		file, handler, err := r.FormFile("image")
-		if err != nil {
-			return err
-		}
-		defer file.Close()
+func (s *Server) handleFile(r *http.Request) (string, error) {
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("image")
 
-		f, err := os.OpenFile("./assets/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-
-		if err != nil {
-			return err
-		}
-		io.Copy(f, file)
-		return writeJSON(w, http.StatusOK, "./assets/"+handler.Filename)
+	if err != nil {
+		return "", err
 	}
-	return writeJSON(w, http.StatusMethodNotAllowed, "Method not allowed!")
+	defer file.Close()
 
+	f, err := os.OpenFile("./assets/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+
+	if err != nil {
+		return "", err
+	}
+	io.Copy(f, file)
+	return "/assets/" + handler.Filename, nil
 }
 
 func (s *Server) handleMoviesById(w http.ResponseWriter, r *http.Request) error {
@@ -121,7 +122,7 @@ func (s *Server) handleMoviesById(w http.ResponseWriter, r *http.Request) error 
 		}
 		return writeJSON(w, http.StatusOK, "Updated!")
 	}
-	if r.Method == "DELETE" {
+	if r.Method == "DELETE" || r.Method == "OPTIONS" {
 		return s.Storage.DeleteMovie(intId)
 	}
 	return writeJSON(w, http.StatusMethodNotAllowed, "Method not allowed!")
